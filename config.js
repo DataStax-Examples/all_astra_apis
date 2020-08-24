ad9f37317ed933ed29829444ab861bdbe2190946
Rebecca Mills <beccamills86@gmail.com>
2020-08-24 13:11:19 -0230

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const mappings = new DefaultTableMappings
 
 /** globally shared configuration values **/
 
-const baseURL = "https://38adb487-e5ae-4aa6-9549-f3c9eb189df5-us-east1.apps.astra.datastax.com"
+const baseURL = "https://<cluster_id_and_region>.apps.astra.datastax.com"
 const restURL = baseURL + "/api/rest"
 const gqlURL =  baseURL + "/api/graphql"
 const secure_bundle_path = "/Users/your.account/Development/all_astra_apis/secure-connect-stuff.zip"
